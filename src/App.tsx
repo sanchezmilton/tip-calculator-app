0297733896252c317bd5ae7d60b678b3072a1d5d
sanchezmilton <sanchezgonzalezmilton@gmail.com>
2023-03-16 21:26:49 -0300

    ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
     saveValue: Function
   ) => (
     <input
-      className={`w-full rounded-lg bg-neutral-100 ${icon} bg-[center_left_1rem] bg-no-repeat px-4 py-2 text-right text-2xl text-neutral-500`}
+      className={`w-full rounded-lg border-neutral-400 bg-neutral-100 ${icon} bg-[center_left_1rem] bg-no-repeat px-4 py-2 text-right text-2xl text-neutral-500`}
       type="number"
       placeholder={placeholder}
       onChange={(e) => saveValue(e.target.value)}
@@ -45,18 +45,18 @@ function App() {
   const resultText = (title: string, subtitle: string, result: number) => (
     <div className="grid grid-cols-2">
       <span className="text-sm text-white">{title}</span>
-      <span className="row-span-2 text-right text-3xl text-neutral-200">
+      <span className="row-span-2 text-right text-3xl text-neutral-400">
         ${result}
       </span>
       <span className="text-xs text-neutral-300">{subtitle}</span>
     </div>
   );
   return (
-    <div className="flex h-screen w-full flex-col justify-end bg-neutral-200 font-space-mono text-neutral-400">
-      <div className="m-auto">
+    <div className="flex h-screen w-full flex-col justify-end bg-neutral-200 font-space-mono text-neutral-300">
+      <div className="m-auto py-8">
         <img src="./logo.svg" alt="Logo" />
       </div>
-      <form className="flex w-full flex-col gap-5 rounded-t-3xl bg-white p-10">
+      <form className="flex h-full w-full flex-col justify-between rounded-t-3xl bg-white p-10">
         <div className="w-full">
           <div>Bill</div>
           {inputText("bg-icon-dollar", "0", setBill)}
@@ -76,13 +76,16 @@ function App() {
           <div>Number of People</div>
           {inputText("bg-icon-person", "0", setNumPeople)}
         </div>
-        <div className="w-full rounded-lg bg-neutral-500 p-5">
+        <div className="flex w-full flex-col gap-7 rounded-xl bg-neutral-500 px-5 pt-8 pb-5">
           {resultText(
             "Tip Amount",
             "/ person",
             Math.round(tipAmountPerson() * 100) / 100
           )}
           {resultText("Total", "/ person", 0)}
+          <button className="rounded-md bg-neutral-400 py-2 text-neutral-500">
+            RESET
+          </button>
         </div>
       </form>
     </div>
